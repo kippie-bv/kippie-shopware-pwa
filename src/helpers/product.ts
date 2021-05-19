4580b10f316ae2719de0bc7ab57f1fdd51457a8d
Mees Hoogendoorn <meeshoogendoorn@Meess-Mac-mini.local>
2021-05-19 08:21:56 +0200

    Added generateShippingMethodLabel helper"

---
+++ src/helpers/product.ts
@@ -0,0 +1,24 @@
+const generateShippingMethodsLabel = (
+  shippingMethodsFromProduct: any[],
+  shippingMethods: any[],
+) => {
+  if (shippingMethodsFromProduct === undefined) {
+    return 'Niet Beschikbaar'
+  }
+  let generatedText = ''
+
+  shippingMethods.forEach((shippingMethod) => {
+    if (!shippingMethodsFromProduct.includes(shippingMethod.id)) {
+      return
+    }
+    if (generatedText.length === 0) {
+      generatedText = generatedText.concat(shippingMethod.name)
+    } else {
+      generatedText = generatedText.concat(' en ').concat(shippingMethod.name)
+    }
+  })
+
+  return generatedText
+}
+
+export { generateShippingMethodsLabel }
