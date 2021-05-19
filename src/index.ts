a10cb7008e90037bd14131d2ae30dd945c9c5f2d
Mees Hoogendoorn <meeshoogendoorn@Meess-Mac-mini.local>
2021-05-19 13:35:38 +0200

    fixed composition-api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { useStores } from './useStores'
 import { useExtendedCart } from './useExtendedCart'
 import { useProductRecommendationModal } from './useProductRecommendationModal'
+import VueCompositionApi from '@vue/composition-api'
+import Vue from 'vue'
+Vue.use(VueCompositionApi)
 
 export * from './shopware-6-client'
 export { useStores, useExtendedCart, useProductRecommendationModal }
