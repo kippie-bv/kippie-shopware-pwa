eaf580dae1fb57f2775eaf5917021a6fdac212a4
Mees Hoogendoorn <meeshoogendoorn@Meess-Mac-mini.local>
2021-06-01 14:18:07 +0200

    Removed redundant composable

--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@ import { useStores } from './useStores'
 import { useExtendedCart } from './useExtendedCart'
 import { useDepositModal } from './useDepositModal'
 import { useModal } from './useModal'
-import { useCustomListingComposable } from './useCustomListingComposable'
 
-export {
-  useStores,
-  useExtendedCart,
-  useDepositModal,
-  useModal,
-  useCustomListingComposable,
-}
+export { useStores, useExtendedCart, useDepositModal, useModal }
