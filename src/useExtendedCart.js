b2dc1bcb02295d13b56cd152a1aef2138f253034
Mees Hoogendoorn <m.hoogendoorn@krs.nl>
2021-07-14 09:21:32 +0200

    v1.0.35

--- src/useExtendedCart.js
+++ src/useExtendedCart.js
@@ -1,50 +1,61 @@
 import { computed } from '@vue/composition-api'
 import { useCart } from '@shopware-pwa/composables'
 
-import {
-  getProductRegularPrice,
-  getProductSpecialPrice,
-  getProductTierPrices,
-  getProductCalculatedListingPrice,
-  getProductCalculatedPrice,
-  getProductPriceDiscount,
-} from '@shopware-pwa/helpers'
-
 const useExtendedCart = (rootContext) => {
   const { cart } = useCart(rootContext)
 
-  const depositProducts = computed(() =>
-    cart.value?.lineItems.filter((lineItem) => {
-      if (lineItem.payload?.type === 'borg') return true
-      return false
-    }),
+  const depositProducts = computed(
+    () =>
+      cart.value?.lineItems.filter((lineItem) => {
+        if (lineItem.payload?.type === 'borg') return true
+        return false
+      }) || [],
   )
 
-  const productsExcludedDeposit = computed(() =>
-    cart.value?.lineItems.filter((lineItem) => {
-      if (lineItem.payload?.type === 'borg') return false
-      return true
-    }),
+  const productsExcludedDeposit = computed(
+    () =>
+      cart.value?.lineItems.filter((lineItem) => {
+        if (lineItem.payload?.type === 'borg') return false
+        return true
+      }) || [],
   )
 
   const totalDepositProducts = computed(() => {
-    return depositProducts.value?.reduce((prev, cur) => {
-      return prev + cur.quantity
-    }, 0)
+    try {
+      depositProducts.value?.reduce((prev, cur) => {
+        return prev + cur.quantity
+      }, 0)
+    } catch (e) {
+      console.error('[useExtendedCart]', 'Total Deposit Products is empty')
+      return 0
+    }
   })
 
   const totalDepositRelatedProducts = computed(() => {
-    return cart.value?.lineItems
-      .filter((lineItem) => lineItem.payload.productNumber.includes('KP-'))
-      .reduce((prev, cur) => {
-        return prev + cur.quantity
-      }, 0)
+    try {
+      return cart.value?.lineItems
+        .filter((lineItem) => lineItem.payload.productNumber.includes('KP-'))
+        ?.reduce((prev, cur) => {
+          return prev + cur.quantity
+        }, 0)
+    } catch (e) {
+      console.error(
+        '[useExtendedCart]',
+        'Total Deposit Related Products is empty',
+      )
+      return 0
+    }
   })
 
   const getTotalPriceOfProductsExcludedDeposit = computed(() => {
-    return productsExcludedDeposit.value.reduce((prev, cur) => {
-      return prev + cur.price.totalPrice
-    }, 0.0)
+    try {
+      return productsExcludedDeposit.value?.reduce((prev, cur) => {
+        return prev + cur.price.totalPrice
+      }, 0.0)
+    } catch (e) {
+      console.error('[useExtendedCart]', 'Total Price unknown')
+      return 0.0
+    }
   })
 
   return {
